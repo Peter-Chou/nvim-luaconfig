39fc5204b02015386c50a4905f7b5b14364ccc14
Peter-Chou <zhou_00009@126.com>
2021-11-29 10:10:08 +0800

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,4 +3,8 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'arcticicestudio/nord-vim'
   use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
 end)
