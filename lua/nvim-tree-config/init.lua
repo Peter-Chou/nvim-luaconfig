3c5db236d87fc72793bb89546e688d63b25c1dec
Peter-Chou <zhou_00009@126.com>
2021-12-02 13:55:41 +0800

    update

--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -3,6 +3,6 @@ require'nvim-tree'.setup {
   update_cwd = true,
   update_focused_file = {enable = true, update_cwd = true},
   view = {width = 25},
-  git = {enable = true, ignore = true, timeout = 500}
+  git = {enable = true, ignore = false, timeout = 500}
 }
 vim.g.nvim_tree_indent_markers = 1
